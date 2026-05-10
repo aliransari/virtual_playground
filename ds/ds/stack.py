b4f55bdcaa0db27333140b1b6dbd0105d02a4e6f
Ali Ansari <ali.ansari.3101@gmail.com>
2026-05-10 20:24:19 +0100

    venv_pytest - a  pytest tutorial

---
+++ ds/ds/stack.py
@@ -0,0 +1,24 @@
+#/usr/bin/env python3
+
+"""
+Demonstrating a Test Driven Deelopment or TDD
+"""
+#pylint: disable=unnecessary-pass, too-few-public-methods
+
+class Stack:
+    """A stack class."""
+    def __init__(self):
+        """Initialisation."""
+        self._storage = []
+    def __len__(self):
+        """The length function."""
+        return len(self._storage)
+    def push(self, item):
+        """Push into stack."""
+        self._storage.append(item)
+    def pop(self):
+        """Pop method."""
+        try:
+            return self._storage.pop()
+        except IndexError:
+            return None
---
+++ ds/tests/test_stack.py
@@ -0,0 +1,33 @@
+#/usr/bin/env python3
+
+import pytest
+from ds.stack import Stack
+
+# create ficxtures to avoid duplication s = Stack() in ever test function
+@pytest.fixture
+def stack():
+    """Applying fixture to avoind duplicate s = Stack(). stack is ow the instance of our class."""
+    return Stack()
+
+
+# testing the constructor which is the name of the class: Stack
+def test_constructor():
+    """Testing the constructor."""
+    s = Stack()
+    assert isinstance(s, Stack)
+    assert s.__class__.__name__ == "Stack"
+    assert len(s) == 0
+
+def test_push(stack):
+    """Push item into stack"""
+    stack.push(3)
+    assert len(stack) == 1
+    stack.push(5)
+    assert len(stack) == 2
+
+def test_pop(stack):
+    stack.push("hello")
+    stack.push("world")
+    assert stack.pop() == "world"
+    assert stack.pop() == "hello"
+    assert stack.pop() is None
